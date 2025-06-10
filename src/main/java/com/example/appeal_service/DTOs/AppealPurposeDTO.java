f8bac5746a10a1dd3585cb5c0d80b4a1ab3e326c
Qoncha A. Abbasova <QAAbbasova@bankofbaku.com>
2025-06-10 17:58:15 +0400

    add method

--- src/main/java/com/example/appeal_service/DTOs/AppealPurposeDTO.java
+++ src/main/java/com/example/appeal_service/DTOs/AppealPurposeDTO.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class AppealPurposeDTO {
+    private Long id;
     private String name;
     private Long categoryId;
 }
--- src/main/java/com/example/appeal_service/controller/AppealPurposeController.java
+++ src/main/java/com/example/appeal_service/controller/AppealPurposeController.java
@@ -3,10 +3,9 @@ package com.example.appeal_service.controller;
 import com.example.appeal_service.DTOs.AppealPurposeDTO;
 import com.example.appeal_service.services.AppealPurposeService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/subcategories")
@@ -18,6 +17,14 @@ public class AppealPurposeController {
     @PostMapping()
     public AppealPurposeDTO createAppealPurpose(@RequestBody AppealPurposeDTO appealPurposeDTO) {
         return appealPurposeService.createAppealPurpose(appealPurposeDTO);
+    }
+    @GetMapping("/{id}")
+    public AppealPurposeDTO getAppealPurposeById(@PathVariable(name = "id") Long id) {
+        return appealPurposeService.getAppealPurposeById(id);
+    }
+    @GetMapping()
+    public List<AppealPurposeDTO> getAppealAll(){
+        return appealPurposeService.getAllAppealPurposes();
     }
 
 }
